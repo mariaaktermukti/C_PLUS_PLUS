058253f07e6090a621f44b371d4eece955e74812
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-16 23:41:11 +0600

    printChar.cpp

---
+++ printChar.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{   
+    int x=10;
+    char c='M';
+    double d= 34.46;
+    std :: cout << x <<std::endl <<c <<std::endl << d << std::endl;
+    std::cout << x << " " << c << std::endl << d << std::endl;
+    return 0;
+}
