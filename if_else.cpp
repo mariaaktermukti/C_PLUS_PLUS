aae5bd3599a911641358c5da396a7ab5f2a91954
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-17 08:11:25 +0600

    if_else.cpp

---
+++ if_else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=9;
+    if(x%2==0)
+    {
+        cout<<"even\n";
+    }
+    else
+    {
+        cout<<"Odd\n";
+    }
+    return 0;
+}
