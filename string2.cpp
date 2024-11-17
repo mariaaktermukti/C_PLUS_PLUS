4407a490195ef321b3f6402653109966137afdce
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-17 08:58:55 +0600

    string1.cpp

---
+++ string2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    cin.ignore();
+    char s[100];
+    cin.getline(s,100);
+    cout<<x<<endl<<s<<endl;
+    return 0;
+}
