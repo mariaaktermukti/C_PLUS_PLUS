6187f914122c0e116d038ac329eb8e962821b608
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-17 08:45:03 +0600

    max.cpp

---
+++ max.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a,b,c,d,e;
+    cin>> a>> b;
+    cout<< max({10, 34, 567, 567,3454})<<endl;
+    cout<<min({10, 34, 567, 567,3454}) << endl;
+    return 0;
+}
