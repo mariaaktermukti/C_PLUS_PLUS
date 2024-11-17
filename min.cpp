05e65150ddb8493d0762c1ba65683a2fa66f4232
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-17 08:41:24 +0600

    min.cpp

---
+++ min.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a,b;
+    cin>> a>> b;
+    // if(a<b){
+    //     cout<<a<<endl;
+    // }
+    // else{
+    //     cout<<b<<endl;
+    // }
+
+    cout<< min(a,b)<<endl;
+    return 0;
+}
