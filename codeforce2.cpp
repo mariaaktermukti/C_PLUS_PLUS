856dd9373c86e0efd53c95dbb3747855b09e575d
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-19 08:49:20 +0600

    codeforce2.cpp

---
+++ codeforce2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    // Reading input
+    cin >> a >> b >> c >> d >> e;
+
+    // Printing each element on a new line
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(2) << d << endl; // Float with 2 decimal places
+    cout << fixed << setprecision(5) << e << endl; // Double with 5 decimal places
+
+    return 0;
+}
