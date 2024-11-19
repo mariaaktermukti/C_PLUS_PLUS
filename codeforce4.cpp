3b306e603f9ed67039972d18534e2b66982684c1
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-19 10:23:20 +0600

    codeforce4.cpp

---
+++ codeforce4.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double R;
+    cin >> R;
+
+    cout << fixed << setprecision(9);
+
+    double area= 3.141592653* R * R;
+    cout<< area << endl;
+    return 0;
+}
