889b4e42a2541f88cd08f1f79fb9cef2bea43f4a
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-20 20:13:38 +0600

    this_and_arrow_sign.cpp

---
+++ this_and_arrow_sign.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+ //constructor->1. constructor class er vitrei take,2. er return type thake na,3.er nam hy exactly class er nam e
+    Student(int roll, int cls, double gpa) // diff perameter na dile must "this" use kora lagbe, other wise na krleu prb nai
+    {
+        (*this).roll=roll;
+        (*this).cls =cls;
+        (*this).gpa= gpa;
+
+    } 
+
+};
+
+int main()
+{
+    Student Rahim(45,5,3.3);
+
+    Student karim(2,5,5.00);
+
+   cout<< Rahim.roll << " " << Rahim.cls << " " << Rahim.gpa << endl;
+   cout<< karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+
+}
