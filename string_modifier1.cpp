7f52078f39662494550a51a75485ac819b3b8843
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-24 08:31:02 +0600

    string_modifier1.cpp

---
+++ string_modifier1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string s="Hello ";
+   string s1= "World";
+   s=s+s1;
+// s.append(s1);
+   cout<< s <<" " <<endl;
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-#include <iostream>
-// #include <string>
+   s=s+s1;
