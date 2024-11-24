18bea7ef269853e258f40e91c92f15be0142d00e
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-24 07:42:49 +0600

    string4.cpp

---
+++ string4.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // char s[100]="Hello";
+    // strcpy(s,"Gello");
+    string s="Hello";
+    s="Hi! how are you?";
+    cout<<s<<endl;
+    return 0;
+}
