37c6e8ed50d5f00babc25db9490d1318d78a4785
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-24 07:58:33 +0600

    string_capacity.cpp

--- string_capacity.cpp
+++ string_capacity.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main(){
     string s="Hello World! This is Maria Akter Mukti from Bangladesh as a CSE engineer.";
-    cout<<s.size()<<endl;
+    // cout<<s.size()<<endl;
+    s.clear();
+    if(s.empty()==true)
+    {
+        cout<<" Empty " << endl;
+    }
+    else
+    {
+        cout<< "Not Empty " << endl;
+    }
+
     return 0;
 }
