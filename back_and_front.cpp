08d80b42be2039638100b508e41cad46de73fdc6
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-24 08:23:27 +0600

    back_and_front.cpp

---
+++ back_and_front.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+//space matter
+int main(){
+   string s;
+   cin>> s;
+    cout<<s.front()<< endl;
+   cout<< s.back() << endl;
+  
+    return 0;
+}
