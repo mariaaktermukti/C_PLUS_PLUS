f07df46dff748747feab5189f6c0c825e3d81562
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-24 08:09:24 +0600

    string_resize.cpp

---
+++ string_resize.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="Hello World! This is Maria Akter Mukti from Bangladesh as a CSE engineer.";
+    s.resize(75,'X');
+    cout<< s << endl;
+    return 0;
+}
