e8a4546fdb5937df741b7635a91cf3cf97f30596
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-26 19:33:50 +0600

    string_input_with_space.cpp

---
+++ string_input_with_space.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin>> x;
+   string s;
+   cin.ignore();
+   getline(cin,s);
+   cout<< x << endl;
+   cout<< s << endl;
+//    cin>> s;
+//    cin.getline(s,100); 
+//    getline(cin,s);
+
+
+//    cin>>s;
+//    cout << x << s << endl;
+
+    return 0;
+}
