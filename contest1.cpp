9099b43e4804b1286f4922a188baba1e931dd04f
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-30 20:47:45 +0600

    contest1.cpp

---
+++ contest1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long findSmallestM(int a, int b)
+{
+    long long m= max(a,b);
+    while(m%a !=m %b)
+    {
+        m++;
+    }
+    return m;
+}
+
+int main()
+{
+    int t;
+    cin>> t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a >> b;
+        cout << findSmallestM(a,b) << endl;
+    }
+    return 0;
+
+}
