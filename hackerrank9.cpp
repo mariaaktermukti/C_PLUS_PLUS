9a13c9e6e5e8579c67dea2fc3bec7f7d9ecc80f6
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-12-13 00:03:46 +0600

    hackerrank9.cpp

--- hackerrank9.cpp
+++ hackerrank9.cpp
@@ -4,24 +4,20 @@ using namespace std;
 
 int main() {
     int N, M, X; 
-
     cin >> N;
     vector<int> A(N);
     for (int i = 0; i < N; i++) {
         cin >> A[i];
     }
-
     cin >> M;
     vector<int> B(M);
     for (int i = 0; i < M; i++) {
         cin >> B[i];
     }
-
     cin >> X;
-
     A.insert(A.begin() + X, B.begin(), B.end());
 
-    for (int i = 0; i < A.size(); i++) {
+    for (size_t i = 0; i < A.size(); i++) {  
         cout << A[i] << " ";
     }
     cout << endl;
