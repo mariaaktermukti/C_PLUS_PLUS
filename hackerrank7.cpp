6864036ef68f44039c57d17c2d07a06d57cee54c
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-12-12 23:16:56 +0600

    hackerrank7.cpp

--- hackerrank7.cpp
+++ hackerrank7.cpp
@@ -7,16 +7,18 @@ int main() {
     int n;
     cin >> n; 
 
-    vector<int> a(n);   
-    vector<int> prefix(n); 
+    vector<long long int> a(n);  
+    vector<long long int> prefix(n); 
 
     for (int i = 0; i < n; i++) {
-        cin >> a[i]; 
+        cin >> a[i];
     }
+
     prefix[0] = a[0];
     for (int i = 1; i < n; i++) {
         prefix[i] = prefix[i - 1] + a[i];
     }
+
     reverse(prefix.begin(), prefix.end());
 
     for (int i = 0; i < n; i++) {
