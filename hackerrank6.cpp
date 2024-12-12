efdde21e1b3d4a17482a5f42763f2f9aad82d039
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-12-12 22:59:34 +0600

    hackerrank6.cpp

--- hackerrank6.cpp
+++ hackerrank6.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n; 
+    int N;
+    cin >> N; 
 
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    vector<int> Arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> Arr[i];
     }
-    sort(arr.begin(), arr.end());
-    for (int i = 1; i < n; i++) {
-        if (arr[i] == arr[i - 1]) {
+    sort(Arr.begin(), Arr.end());
+    for (int i = 1; i < N; i++) {
+        if (Arr[i] == Arr[i - 1]) {
             cout << "YES" << endl; 
             return 0; 
         }
