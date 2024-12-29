0b5e14da4d02f15bb761ce6de25499e3def9cb68
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-12-29 19:39:39 +0600

    delete_at_head.cpp

--- delete_at_head.cpp
+++ delete_at_head.cpp
@@ -36,6 +36,12 @@ void print_linked_list(Node* head)
     }
 }
 
+void delete_at_head(Node* &head)
+{
+    Node* deleteNode= head;
+    head=head->next;
+    delete deleteNode;
+}
 int main()
 {
     Node* head= NULL;
@@ -51,6 +57,7 @@ int main()
         }
         insert_at_tail(head,tail,val);
     }
+    delete_at_head(head);
     print_linked_list(head);
 
     return 0;
