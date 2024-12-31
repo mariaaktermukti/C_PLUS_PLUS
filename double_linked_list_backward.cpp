e41b76880a2d6a801f1ad18e00269f288d1eef8a
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-12-31 09:05:12 +0600

    doubly_linked_list_backward.cpp

---
+++ double_linked_list_backward.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+       int val;
+       Node* next;
+       Node* prev;
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }   
+};
+void print_backward(Node* tail)
+{
+    Node* tmp=tail;
+    while(tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp=tmp->prev;
+    }
+    cout << endl;
+}
+ 
+int main()
+{
+    Node* head= new Node(10);
+    Node* a= new Node(20);
+    Node* tail= new Node(30);
+    
+    head->next=a;
+    a->prev=head;
+
+    a->next=tail;
+    tail->prev=a;
+
+    print_backward(tail);
+    return 0;
+}
