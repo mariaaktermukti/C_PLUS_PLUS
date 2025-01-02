59bf83cf70a325b3df2587a0fb24c246ab4ed06b
mariaaktermukti <mariamukti051@gmail.com>
2025-01-02 23:44:57 +0600

    hackerrank16.cpp

--- hackerrank16.cpp
+++ hackerrank16.cpp
@@ -1,10 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() 
-{
+int main() {
     list<int> linkedList;
-    set<int> uniqueValues;
+    list<int> uniqueList;  
     int value;
 
     while (cin >> value && value != -1)
@@ -12,25 +11,22 @@ int main()
         linkedList.push_back(value);
     }
 
-    for (int val : linkedList)
+    for (int val : linkedList) 
     {
-        uniqueValues.insert(val);
+        if (find(uniqueList.begin(), uniqueList.end(), val) == uniqueList.end())
+        {
+            uniqueList.push_back(val); 
+        }
     }
 
-    linkedList.clear();
-    for (int val : uniqueValues) 
-    {
-        linkedList.push_back(val);
-    }
+    uniqueList.sort();
 
-    linkedList.sort();
-
-    for (int val : linkedList)
+    for (int val : uniqueList) 
     {
         cout << val << " ";
     }
 
     cout << endl;
-    
+
     return 0;
 }
