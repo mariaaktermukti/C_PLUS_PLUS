6e900056f32889ae0822a4aa298075fc48ab145f
mariaaktermukti <mariamukti051@gmail.com>
2025-01-05 08:33:03 +0600

    implement_stack_using_array.cpp

--- taskManager_leetcode.cpp
+++ taskManager_leetcode.cpp
@@ -1,65 +1,69 @@
 #include <iostream>
-#include <unordered_map>
-#include <set>
 #include <vector>
+#include <unordered_map>
+#include <queue>
+#include <tuple>
 using namespace std;
+
+struct Task {
+    int userId;
+    int taskId;
+    int priority;
+};
 
 class TaskManager {
 private:
-    struct Task {
-        int userId;
-        int priority;
-    };
-
-    unordered_map<int, Task> taskMap;
-    set<pair<int, int>> prioritySet;
+    unordered_map<int, Task> taskMap; 
+    priority_queue<tuple<int, int, int>> taskQueue; 
 
 public:
     TaskManager(vector<vector<int>>& tasks) {
-        for (const auto& task : tasks) {
-            int userId = task[0], taskId = task[1], priority = task[2];
-            add(userId, taskId, priority);
+        for (const auto& t : tasks) {
+            add(t[0], t[1], t[2]);
         }
     }
 
     void add(int userId, int taskId, int priority) {
-        taskMap[taskId] = {userId, priority};
-        prioritySet.insert({-priority, taskId});
+        Task task = {userId, taskId, priority};
+        taskMap[taskId] = task; 
+        taskQueue.emplace(priority, taskId, userId); 
     }
 
     void edit(int taskId, int newPriority) {
-        auto it = taskMap.find(taskId);
-        if (it != taskMap.end()) {
-            int oldPriority = it->second.priority;
-            prioritySet.erase({-oldPriority, taskId});
-            it->second.priority = newPriority;
-            prioritySet.insert({-newPriority, taskId});
+        if (taskMap.find(taskId) != taskMap.end()) {
+            Task& task = taskMap[taskId];
+            task.priority = newPriority;
+            taskQueue.emplace(newPriority, taskId, task.userId); 
         }
     }
 
     void rmv(int taskId) {
-        auto it = taskMap.find(taskId);
-        if (it != taskMap.end()) {
-            int priority = it->second.priority;
-            prioritySet.erase({-priority, taskId});
-            taskMap.erase(it);
+        if (taskMap.find(taskId) != taskMap.end()) {
+            taskMap.erase(taskId); 
         }
     }
 
     int execTop() {
-        if (prioritySet.empty()) {
-            return -1;
+        while (!taskQueue.empty()) {
+            auto [priority, taskId, userId] = taskQueue.top();
+            taskQueue.pop();
+
+            if (taskMap.find(taskId) != taskMap.end() && taskMap[taskId].priority == priority) {
+                taskMap.erase(taskId); 
+                return userId; 
+            }
         }
-        auto top = *prioritySet.begin();
-        int taskId = top.second;
-        int userId = taskMap[taskId].userId;
-        rmv(taskId); 
-        return userId;
+        return -1; 
     }
 };
 
 int main() {
-    vector<vector<int>> tasks = {{1, 101, 10}, {2, 102, 20}, {3, 103, 15}};
+    vector<vector<int>> tasks = {
+        {1, 101, 10}, 
+        {2, 102, 20}, 
+        {3, 103, 15}
+    };
+
     TaskManager taskManager(tasks);
 
     taskManager.add(4, 104, 5);
