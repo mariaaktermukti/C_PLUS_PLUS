311e50057638fc683fd968cc3a238da8c1dfb410
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-01-14 00:57:46 +0600

    Add files via upload

--- minimum_length_of_string_after_operations_leetcode.cpp
+++ minimum_length_of_string_after_operations_leetcode.cpp
@@ -7,22 +7,16 @@ public:
     int minimumLength(string s) {
         int left = 0, right = s.length() - 1;
 
-        // Use two pointers to shrink the string from both ends
         while (left < right && s[left] == s[right]) {
             char currentChar = s[left];
-
-            // Move the left pointer to skip characters matching the currentChar
             while (left <= right && s[left] == currentChar) {
                 left++;
             }
-
-            // Move the right pointer to skip characters matching the currentChar
             while (left <= right && s[right] == currentChar) {
                 right--;
             }
         }
 
-        // The remaining string length
         return right - left + 1;
     }
 };
@@ -30,15 +24,13 @@ public:
 int main() {
     Solution solution;
 
-    // Example 1
     string s1 = "abaacbcbb";
     cout << "Input: " << s1 << "\nOutput: " << solution.minimumLength(s1) << "\n\n";
 
-    // Example 2
+
     string s2 = "aa";
     cout << "Input: " << s2 << "\nOutput: " << solution.minimumLength(s2) << "\n\n";
 
-    // Additional Test Cases
     string s3 = "a";
     cout << "Input: " << s3 << "\nOutput: " << solution.minimumLength(s3) << "\n\n";
 
