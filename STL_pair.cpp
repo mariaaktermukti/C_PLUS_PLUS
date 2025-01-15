b0e9da84aaf132a137bd264dcbefaa5332815976
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-01-15 19:34:17 +0600

    Add files via upload

---
+++ STL_pair.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>> n;
+    vector<pair<int,int>> v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>> v[i].first >> v[i].second;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        cout<< v[i].first << " " << v[i].second << endl;
+    }
+
+    // pair<string, string> p;
+    // p= make_pair("maria", "mukti");
+    // cout << p.first << endl;
+    // cout << p.second << endl;
+    return 0;
+}
