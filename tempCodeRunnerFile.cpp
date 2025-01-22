27409c01353d594b830106c209797fb3826980cf
mariaaktermukti <mariamukti051@gmail.com>
2025-01-22 19:20:07 +0600

    dota2_senate_leetcode.cpp

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) 
+    {
+        queue<int> radiant, dire;
+        int n = senate.size();
+
+        for (int i = 0; i < n; ++i) 
+        {
+            if (senate[i] == 'R') 
+            {
+                radiant.push(i);
+            } 
+            else 
+            {
+                dire.push(i);
+            }
+        }
+
+        while (!radiant.empty() && !dire.empty()) 
+        {
+            int r_index = radiant.front();
+            int d_index = dire.front();
+            radiant.pop();
+            dire.pop();
+
+            if (r_index < d_index) 
+            {
+                radiant.push(r_index + n); 
+            } 
+            else 
+            {
+                dire.push(d_index + n);
+            }
+        }
+        return radiant.empty() ? "Dire" : "Radiant";
+    }
+};
